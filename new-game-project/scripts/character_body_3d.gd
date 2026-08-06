97cdf05022fb2265f0cdab814d5efc4b59c8ffa7
Michael Hayes <michael.hayes@marcellin.school.nz>
2026-08-07 11:01:01 +1200

    Tweaking Values
    Tweaking some of the unused variables

--- new-game-project/scripts/character_body_3d.gd
+++ new-game-project/scripts/character_body_3d.gd
@@ -85,7 +85,7 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("left_click"):
 		var bullet = Bullet.instantiate()
 		get_tree().current_scene.add_child(bullet)
--- new-game-project/scripts/dummy.gd
+++ new-game-project/scripts/dummy.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 
 @onready var mat = $MeshInstance3D.get_active_material(0) as StandardMaterial3D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var space_state = get_world_3d().direct_space_state
 	var mousepos = get_viewport().get_mouse_position()
 
