1d333ca1937062a0458b51b3057587df76efdf10
Michael Hayes <michael.hayes@marcellin.school.nz>
2026-08-19 11:04:26 +1200

    Card Blueprints
    Created a functioning template system that provides identity upon cards

--- new-game-project/scripts/card_template.gd
+++ new-game-project/scripts/card_template.gd
@@ -6,7 +6,7 @@ var enabled = true
 var CardData = [
 	{
 		"name" : "ATK_SPEED",
-		"description" : "Increase Attack Speed by 20%",
+		"description" : "Increase Attack Speed by 5%",
 		"color" : Color(1.0, 1.0, 1.0, 1.0),
 		"tier" : "Common",
 		"emblem_texture" : Color() # Change color to texture when pngs have been uploaded
@@ -14,31 +14,51 @@ var CardData = [
 	{
 		"name" : "ATK_SPEED",
 		"description" : "Increase Attack Speed by 20%",
-		"color" : Color(1.0, 0.0, 0.0, 1.0),
-		"tier" : "Common",
+		"color" : Color(0.223, 0.597, 0.626, 1.0),
+		"tier" : "Uncommon",
 		"emblem_texture" : Texture2D
 	}
 ]
 
-func _ready() -> void:
-	var Card_ID = CardData.pick_random()
-	var Card_name = Card_ID["name"]
-	var Card_desc = Card_ID["description"]
-	var Card_color = Card_ID["color"]
-	var Card_tier = Card_ID["tier"]
-	var Card_emblem = Card_ID["emblem_texture"]
+var Card_name
+var Card_desc 
+var Card_color 
+var Card_tier 
+var Card_emblem
+
+var card_id: int
+var card_name: String
+var card_description: String
+var card_color: Color
+var card_tier: String
+var card_emblem: Color # Will change to texture
+
+func setup_card(id: int, data: Dictionary) -> void:
+	card_id = id
+	card_name = data["name"]
+	card_description = data["description"]
+	card_color = data["color"]
+	card_tier = data["tier"]
+	card_emblem = data["emblem_texture"]
+
+	print("I am card: ", card_name)
+	print("My effect: ", card_description)
 	
-	color = Card_color
-	#$Icon.texture = Card_emblem
-	$Label.text = Card_name
-	$Label2.text = Card_desc
+	$Label.text = card_name
+	$Label2.text = card_description
+	color = card_color
+	$Icon.color = card_emblem
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
 		if enabled:
 			vanity.rpc()
+			modifier_apply()
 		elif !enabled:
 			print("disabled")
+
+func modifier_apply():
+	pass
 
 @rpc("call_local", "any_peer")
 func vanity():
@@ -52,4 +72,4 @@ func _on_mouse_entered() -> void:
 	color = Color(0.815, 0.344, 0.719, 1.0)
 
 func _on_mouse_exited() -> void:
-	color = default_color
+	color = card_color
--- new-game-project/scripts/margin_container.gd
+++ new-game-project/scripts/margin_container.gd
@@ -1,6 +1,58 @@
 extends MarginContainer
 
-@export var card_roster: Array[PackedScene]
+@export var Card_template: PackedScene
+
+var CardData = [
+	{
+		"name": "ATK SPEED",
+		"description": "Increase Attack Speed by 5%",
+		"color": Color(1.0, 1.0, 1.0, 1.0),
+		"tier": "Common",
+		"emblem_texture": Color(1.0, 0.0, 0.0, 1.0) # Will change to a texture
+	},
+	{
+		"name": "ATK SPEED +",
+		"description": "Increase Attack Speed by 20%",
+		"color": Color(0.223, 0.597, 0.626, 1.0),
+		"tier": "Uncommon",
+		"emblem_texture": Color(1.0, 0.0, 0.0, 1.0) # Will change to a texture
+	},
+	{
+		"name": "MOVE SPEED",
+		"description": "Increase Movement Speed by 5%",
+		"color": Color(1.0, 1.0, 1.0, 1.0),
+		"tier": "Common",
+		"emblem_texture": Color(0.0, 0.589, 0.752, 1.0) # Will change to a texture
+	},
+	{
+		"name": "MOVE SPEED +",
+		"description": "Increase Movement Speed by 20%",
+		"color": Color(0.223, 0.597, 0.626, 1.0),
+		"tier": "Uncommon",
+		"emblem_texture": Color(0.0, 0.589, 0.752, 1.0) # Will change to a texture
+	},
+	{
+		"name": "GHOST WALK",
+		"description": "Provides Intangibility for 5 seconds to obstacles",
+		"color": Color(0.813, 0.267, 0.771, 1.0),
+		"tier": "Epic",
+		"emblem_texture": Color(1.0, 1.0, 1.0, 1.0) # Will change to a texture
+	},
+	{
+		"name": "ECHO SHIELD",
+		"description": "Froms a defensive layer around the player providing an extra life",
+		"color": Color(1.0, 0.78, 0.231, 1.0),
+		"tier": "Legendary",
+		"emblem_texture": Color(0.375, 0.001, 0.488, 1.0) # Will change to a texture
+	}
+]
+
+var Card_name
+var Card_desc
+var Card_color
+var Card_tier
+var Card_emblem
+
 
 func _ready() -> void:
 	if multiplayer.is_server():
@@ -10,11 +62,11 @@ func _ready() -> void:
 		rng.randomize()
 
 		for i in range(8):
-			card_ids.append(rng.randi_range(0, card_roster.size() - 1))
-			print(card_ids)
+			card_ids.append(rng.randi_range(0, CardData.size() - 1))
 
 		card_loader.rpc(card_ids)
 
+# Will need to modify later to accomadate for percentage chances on card rarities
 @rpc("call_local", "reliable")
 func card_loader(card_ids: Array[int]) -> void:
 	var index := 0
@@ -26,8 +78,11 @@ func card_loader(card_ids: Array[int]) -> void:
 
 		for column in range(4):
 			var card_id: int = card_ids[index]
+			var card_data: Dictionary = CardData[card_id]
 			index += 1
 
-			var current_card := card_roster[card_id]
-			var loaded_card := current_card.instantiate()
+			var loaded_card = Card_template.instantiate()
+
+			loaded_card.setup_card(card_id, card_data)
+
 			row_load.add_child(loaded_card)
